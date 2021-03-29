16877e338d371867735aacc0197cf8f3104adb2b
Evonnefanfan <79820792+Evonnefanfan@users.noreply.github.com>
2021-03-29 22:40:45 +0800

    329

---
+++ 329.js
@@ -0,0 +1,54 @@
+document.write("say hello!");
+
+function sayhello(){
+var response=prompt("what's your name?");
+alert("hello"+response+",have a good day!");
+}
+
+function add(a,b){
+  return a+b;
+}
+console.log(add(20,30));
+console.log(add(100,30));
+
+function showTopic(){
+  var x=document.getElementById("demo");
+  x.style.fontSize="25px";
+  x.style.color="red";
+}
+
+var movies = ["Avengers","Captain Marvel", "Black panther", "Spiderman"];
+movies[0] = "Wonder woman";
+movies.push("Avengers");
+movies.pop();
+console.log(movies);
+
+
+var movie={
+  title:"Avengers",
+  time:"12pm",
+  status:"availble"};
+
+  var movieDetails = [
+{title: "Avengers",
+  time:"12pm",
+  status: "available"},
+{title: "Captain Marvel",
+  time:"2pm",
+  status: "unavailable"},
+{title: "Black Panther",
+  time:"4pm",
+  status: "available"},
+{title: "Spiderman",
+  time:"6pm",
+  status: "available"}
+];
+
+for (var movie of movieDetails){
+  if (movie.status === "available"){
+    console.log("The movie " + movie.title + " plays at " + movie.time)
+  }
+else{
+    console.log("Sorry, the movie " + movie.title + " is sold out.")
+  }
+};
